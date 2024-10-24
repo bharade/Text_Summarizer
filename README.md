106996b1a94afc19f0b4b1dbcee72f6cdb5b160e
bharade <adibharade@gmail.com>
2024-10-24 22:58:30 +0530

    01_dataingestion_part1

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Text_Summarizer
+# End to End Text_Summarizer Project #
+
+
+## Workflows
+
+1. update config.yaml
+2. update params.yaml
+3. update entity.yaml
+4. update configuration manager in src/config
+5. update the components-> data ingestion,transformation, model trainer
+6. update the pipeline
+7. update the main.py
+8. update the app.py
---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,52 @@
+import os
+from box.exceptions import BoxValueError
+import yaml 
+from textSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from typing import Any
+from pathlib import Path
+
+@ensure_annotations
+def read_yaml(path_to_yaml:Path)->ConfigBox:
+    """
+    Read the yaml file and return the ConfigBox object
+    Args:
+    path_to_yaml: Path to the yaml file
+    Returns:
+    ConfigBox object
+    """
+    try:
+        with open(path_to_yaml, 'r') as file:
+            config = yaml.safe_load(file)
+            config = ConfigBox(config)
+            logger.info(f"YAML file read successfully from {path_to_yaml}")
+            return config
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise 
+    
+@ensure_annotations
+def create_directories(path_to_directories:list, verbose=True):
+    """
+    Create directories if they do not exist
+    Args:
+    path_to_directories: List of paths to directories
+    Returns:
+    None
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+
+        if verbose:
+            logger.info(f"Directory created at {path}")
+
+@ensure_annotations
+def get_size(path:Path)->str:
+    """get size in KB
+    Args: path: Path to the file
+    Returns: size in KB
+    """
+    size_in_kb=round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
