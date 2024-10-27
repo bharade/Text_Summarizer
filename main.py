2a38a2b286732866b22608108df749c81b04024b
bharade <adibharade@gmail.com>
2024-10-27 15:09:10 +0530

    model evaluation completed

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_tranformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -54,3 +55,13 @@ try:
 except Exception as e: 
     logger.exception(e)
     raise e 
+
+STAGE_NAME="Model Evaluation Stage"
+try:
+    logger.info(f">>>stage {STAGE_NAME} started<<<<")
+    model_evaluation=ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
+    logger.info(f">>>stage {STAGE_NAME} completed<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
